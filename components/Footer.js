0408199c8f5ed11cc6f1b625d85de1ee993b9378
hi-radik <practice.radomir@gmail.com>
2022-05-05 21:34:09 +0300

    footer added

--- components/Footer.js
+++ components/Footer.js
@@ -18,18 +18,76 @@ const Wrapper2 = styled.div`
     rgba(241, 242, 244, 0.9) 0%,
     rgba(241, 242, 244, 0) 100%
   ); */
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+`;
+const FooterList = styled.ul`
+  max-width: 150px;
+  margin-left: 135px;
+  padding-top: 25px;
+`;
+const FooterListItem = styled.li`
+  font-size: 16px;
+  color: #292d34;
+  line-height: 2;
+`;
+const FooterAllRights = styled.p`
+  color: #858a92;
+  font-size: 14px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
 `;
 function Footer() {
   return (
     <Footerino>
       <Wrapper2>
-        <Wrapper2>
-          <Link href="/">
-            <a style={{ width: "150px" }}>
-              <Image src="/logo.svg" alt="logo" width="150" height="50" />
-            </a>
-          </Link>
-        </Wrapper2>
+        <Link href="/">
+          <a style={{ width: "150px" }}>
+            <Image src="/logo.svg" alt="logo" width="150" height="50" />
+          </a>
+        </Link>
+        <FooterList>
+          <FooterListItem>
+            <span style={{ color: "#FC6967" }}>События</span>
+          </FooterListItem>
+          <FooterListItem>
+            <Link href="/">
+              <a>День рождения</a>
+            </Link>
+          </FooterListItem>
+          <FooterListItem>
+            <Link href="/">
+              <a>Свадьба</a>
+            </Link>
+          </FooterListItem>
+          <FooterListItem>
+            <Link href="/">
+              <a>Годовщина</a>
+            </Link>
+          </FooterListItem>
+          <FooterListItem>
+            <Link href="/">
+              <a>Выпуск</a>
+            </Link>
+          </FooterListItem>
+          <FooterListItem>
+            <Link href="/">
+              <a>Карьерный рост</a>
+            </Link>
+          </FooterListItem>
+          <FooterListItem>
+            <Link href="/">
+              <a>Благодарность</a>
+            </Link>
+          </FooterListItem>
+        </FooterList>
+        <FooterAllRights>
+          Saluts team 2022.
+          <br />
+          Все права защищены.
+        </FooterAllRights>
       </Wrapper2>
     </Footerino>
   );
