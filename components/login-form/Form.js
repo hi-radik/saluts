aef10cb3860adc95691823110c0fba600f42b098
hi-radik <practice.radomir@gmail.com>
2022-05-10 12:02:42 +0300

    Priva

--- components/login-form/Form.js
+++ components/login-form/Form.js
@@ -13,7 +13,7 @@ const LoginForm = styled.form`
   padding-top: 60px;
   input {
     background-color: #f1f2f4;
-    font-family: "Gotham Pro";
+    font-family: 'Roboto', sans-serif;
     font-size: 15px;
     font-weight: 300;
     width: 400px;
@@ -31,6 +31,7 @@ const LoginForm = styled.form`
     }
   }
   button {
+    font-family: 'Roboto', sans-serif;
     font-weight: 500;
     letter-spacing: 0.7px;
     font-size: 16px;
@@ -49,9 +50,11 @@ const LoginForm = styled.form`
     }
   }
   p {
+    font-family: 'Roboto', sans-serif;
     color: #858a92;
     font-size: 14px;
     a {
+      font-family: 'Roboto', sans-serif;
       color: #fc6967;
       font-size: 14px;
       text-decoration: underline;
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import Head from "next/head";
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
